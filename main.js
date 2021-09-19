5cb4df937621caa06c28eb9453d4a05b62faf357
Cleber Stolz <cleberst_stolz@hotmail.com>
2021-09-19 19:35:23 -0300

    implementando partes e adicionando scroll

--- main.js
+++ main.js
@@ -34,3 +34,36 @@ window.addEventListener('scroll', () => {
         header.classList.remove('scroll');
     }
 })
+
+
+// Testimonials slide swiper
+
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    pagination: {
+        el: '.swiper-pagination'
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
+
+
+  // ScrollReveal: mostrar elementos quando der scroll
+
+  const scrollReveal = ScrollReveal({
+
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+    reset: true
+  });
+
+  scrollReveal.reveal(
+    `#home .img, #home .text
+    #about .image, #about .text,
+    #services header, #services .card,
+    #testimonials header, #testimonials .testimonials,
+    #contact .text, #contact .links
+    `,
+    { interval: 100 }
+)
