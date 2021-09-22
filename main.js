0db13c70a938ada16d67bf85548994cb2277afb4
Cleber Stolz <cleberst_stolz@hotmail.com>
2021-09-22 17:45:09 -0300

    implementando footer e botao back to top

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 //abre e fecha o menu
-
 const nav = document.querySelector('#header nav');
 const toggle = document.querySelectorAll('nav .toggle');
-
 
 for (const element of toggle) {
     element.addEventListener('click', () => {
@@ -11,7 +9,6 @@ for (const element of toggle) {
 }
 
 // quando clicar em um item do menu esconder o menu
-
 const links = document.querySelectorAll('nav ul li a');
 
 for (const link of links) {
@@ -21,11 +18,10 @@ for (const link of links) {
 }
 
 //mudar o header da page quando der scroll
-
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
-
-window.addEventListener('scroll', () => {
+function changeHeaderWhenSroll() {
+    const header = document.querySelector('#header')
+    const navHeight = header.offsetHeight
+    
     if(window.scrollY >= navHeight) {
         //scroll é maior que a altura do header
         header.classList.add('scroll');
@@ -33,11 +29,10 @@ window.addEventListener('scroll', () => {
         // menor que a altura do header
         header.classList.remove('scroll');
     }
-})
 
+}
 
 // Testimonials slide swiper
-
 const swiper = new Swiper('.swiper', {
     slidesPerView: 1,
     pagination: {
@@ -45,25 +40,46 @@ const swiper = new Swiper('.swiper', {
     },
     mousewheel: true,
     keyboard: true,
-  });
+});
 
+// ScrollReveal: mostrar elementos quando der scroll
 
-  // ScrollReveal: mostrar elementos quando der scroll
-
-  const scrollReveal = ScrollReveal({
-
+const scrollReveal = ScrollReveal({
+    
     origin: 'top',
     distance: '30px',
     duration: 700,
     reset: true
   });
-
-  scrollReveal.reveal(
+  
+scrollReveal.reveal(
     `#home .img, #home .text
     #about .image, #about .text,
     #services header, #services .card,
     #testimonials header, #testimonials .testimonials,
     #contact .text, #contact .links
+    footer .brand, footer .social
     `,
     { interval: 100 }
 )
+      
+// back to top para voltar para o top
+
+function backToTop() {
+    
+    const backToTopButton = document.querySelector('.back-to-top')
+    window.addEventListener('scroll', () => {
+        if(window.scrollY >= 560) {
+            backToTopButton.classList.add('show')
+          } else {
+              backToTopButton.classList.remove('show')
+          }
+      })
+
+}
+        
+window.addEventListener('scroll', () => {
+
+    changeHeaderWhenSroll()
+    backToTop()
+})
