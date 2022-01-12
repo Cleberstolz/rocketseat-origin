20600f9d75c346c5c74dba715389a75d2e026ff9
Cleber Stolz <cleberst_stolz@hotmail.com>
2022-01-12 08:16:40 -0300

    finalização da estilização

--- main.js
+++ main.js
@@ -18,10 +18,9 @@ for (const link of links) {
 }
 
 //mudar o header da page quando der scroll
-function changeHeaderWhenSroll() {
-    const header = document.querySelector('#header')
-    const navHeight = header.offsetHeight
-    
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+function changeHeaderWhenSroll() {    
     if(window.scrollY >= navHeight) {
         //scroll é maior que a altura do header
         header.classList.add('scroll');
@@ -29,7 +28,6 @@ function changeHeaderWhenSroll() {
         // menor que a altura do header
         header.classList.remove('scroll');
     }
-
 }
 
 // Testimonials slide swiper
@@ -40,6 +38,12 @@ const swiper = new Swiper('.swiper', {
     },
     mousewheel: true,
     keyboard: true,
+    breakpoints: {
+        767: {
+            slidesPerView: 2,
+            setWrapperSize: true
+        }
+    }
 });
 
 // ScrollReveal: mostrar elementos quando der scroll
@@ -65,9 +69,9 @@ scrollReveal.reveal(
       
 // back to top para voltar para o top
 
+const backToTopButton = document.querySelector('.back-to-top')
 function backToTop() {
     
-    const backToTopButton = document.querySelector('.back-to-top')
     window.addEventListener('scroll', () => {
         if(window.scrollY >= 560) {
             backToTopButton.classList.add('show')
@@ -77,9 +81,36 @@ function backToTop() {
       })
 
 }
-        
-window.addEventListener('scroll', () => {
 
+// Menu ativo para cada seção 
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+
+    const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+    for ( const section of sections ) {
+        const sectionTop = section.offsetTop
+        const sectionHeight = section.offsetHeight
+        const sectionId = section.getAttribute('id')
+
+        const checkpointStart = checkpoint >= sectionTop
+        const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+        if(checkpointStart && checkpointEnd) {
+            document.querySelector('nav ul li a[href*=' + sectionId + ']')
+            document.classList.add('active')
+        } else {
+            document.querySelector('nav ul li a[href*=' + sectionId + ']')
+            document.classList.remove('active')
+        }
+    }
+}
+
+window.addEventListener('scroll', () => {
     changeHeaderWhenSroll()
     backToTop()
+    activateMenuAtCurrentSection()
 })
+
+
+
